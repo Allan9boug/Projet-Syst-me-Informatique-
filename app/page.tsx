5d88efaeba7582430db8bbff70a7cf06ee2c2538
bouguion <bouguion@sio.ndlp>
2025-11-25 16:32:37 +0100

    Ajout de l'affichage des réponses

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { supabase } from '../lib/supabaseClient';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const [question, setQuestion] = useState<any>(null);
@@ -11,8 +12,17 @@ export default function Home() {
     async function fetchQuestion() {
       const { data, error } = await supabase
         .from('question')
-        .select('*');
-        // .limit(1);
+        .select(`
+          id,
+          texte,
+          reponses:reponse (
+            id,
+            texte,
+            rep_correct
+          )
+        `)
+        .order('id', { ascending: true });
+      // .limit(1);
 
       if (error) console.error(error);
       else setQuestion(data[0]); // On stocke la première question dans l’état
@@ -22,30 +32,57 @@ export default function Home() {
     fetchQuestion();
   }, []);
 
-  return (
-  <div>
-    <Alert className="max-w-xl mx-auto mt-4">
-  <AlertTitle>Bienvenue sur CyberQuiz</AlertTitle>
-  <AlertDescription>
-    Préparez-vous à tester vos connaissances !
-  </AlertDescription>
-</Alert>
-    {question ? (
-  <Card className="max-w-xl mx-auto mt-6">
-    <CardHeader>
-      <CardTitle>Question</CardTitle>
-    </CardHeader>
-    <CardContent>
-      <p>{question.texte}</p>
-    </CardContent>
-  </Card>
-  
-) : (
-  <p>Chargement de la question...</p>)
-  
-  
+  function handleClick(reponse: any) {
+    if (reponse.est_correcte) {
+      alert("Bonne réponse !");
+    } else {
+      alert("Mauvaise réponse.");
+    }
   }
-  </div>
+
+  return (
+    <div>
+      <Alert className="max-w-xl mx-auto mt-4">
+        <AlertTitle>Bienvenue sur CyberQuiz</AlertTitle>
+        <AlertDescription>
+          Préparez-vous à tester vos connaissances !
+        </AlertDescription>
+      </Alert>
+
+
+      {question ? (
+        <Card className="max-w-xl mx-auto mt-6">
+          <CardHeader>
+            <CardTitle>Question</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>{question.texte}</p>
+
+
+
+
+
+            {question.reponses.map((reponse: any) => (
+              <Button
+                key={reponse.id}
+                onClick={() => handleClick(reponse)}
+                className="w-full justify-start mt-4"
+                variant="outline"
+              >
+                {reponse.texte}
+              </Button>
+            ))}
+
+          </CardContent>
+        </Card>
+
+
+      ) : (
+        <p>Chargement de la question...</p>)
+
+
+      }
+    </div>
   );
 
 }
